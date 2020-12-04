b1a930ee6695e6e4698ad64ceec9b6810e79e7d6
MultiApollo19 <mariusz.belwon@gmail.com>
2020-12-04 12:14:28 +0100

    Covid Simulator 2k20 Grudziadz edition

--- c++TEst/c++TEst.cpp
+++ c++TEst/c++TEst.cpp
@@ -66,15 +66,21 @@ int main()
 	//CovID simulator 2k20
 	int liczbaZarazonych = 1;
 	int liczbaDni = 0;
-	while (liczbaZarazonych < 94497) {		
+	int liczbaMieszkancow = 94497;
+	int liczbaMartwych = 0;
+	while (liczbaZarazonych < liczbaMieszkancow) {
 		liczbaDni++;
 
 		if (liczbaDni % 7 != 3) {
-			liczbaZarazonych = liczbaZarazonych + 2 * liczbaZarazonych + liczbaZarazonych * 0, 2;
+			liczbaZarazonych = liczbaZarazonych + 2 * liczbaZarazonych + liczbaZarazonych * 0.2;
 		}
 		if (liczbaDni % 7 == 6) {
-			liczbaZarazonych -= liczbaZarazonych * 0, 3;
+			liczbaZarazonych -= liczbaZarazonych * 0.3;
+			liczbaMartwych = liczbaMartwych + (liczbaZarazonych * 0.3);
+			liczbaMieszkancow -= liczbaZarazonych * 0.3;
 		}
+		cout << "Liczba zarazonych: " << liczbaZarazonych << " w "<< liczbaDni << " dniu" << endl;
+		cout << "Liczba martwych: " << liczbaMartwych << endl;
 	}
 	cout << "Liczba dni do momentu wyginiecia Grudziadzan: " << liczbaDni-1 << endl;
 	return 0;
