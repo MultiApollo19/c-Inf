e8d191b8464174f98f5fce7881c8cb8ea0c8d621
MultiApollo19 <mariusz.belwon@gmail.com>
2021-02-12 11:48:54 +0100

    Zadanko 1 i 2

--- tablice/tablice.cpp
+++ tablice/tablice.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 int main() {
-	int tablica[10];
+	/*int tablica[10];
 	for (int i = 0; i < size(tablica); i++)
 	{
 		int random = rand();
@@ -16,5 +16,50 @@ int main() {
 			tablica[i] = 1;
 		}
 		cout << tablica[i] << endl;
+	}*/
+
+
+	int tablica[10];
+	bool isParzysta = false;
+	string tempInput;
+	cout << "Wpisz rodzaj liczby(parzysta/nieparzysta): ";
+	cin >> tempInput;
+	if (tempInput == "parzysta" || tempInput == "Parzysta") {
+		isParzysta = true;
 	}
+	else if (tempInput == "nieparzysta" || tempInput == "Nieparzysta") {
+		isParzysta = false;
+	}
+	else {
+		cout << endl << "Wprowadz poprawna wartosc";
+		return 0;
+	}
+	for (int i = 0; i < size(tablica); i++)
+	{
+		cout << endl << "Debug iterator: " << i;
+		int random = (rand() % 100) + 1;
+		cout << endl <<"Debug random: "<<random;
+		if (isParzysta) {
+			cout << endl << "Debug isParzysta: " << isParzysta;
+			if (random % 2 == 0) {
+				tablica[i] = random;
+				cout << endl << "Index: " << i << " w tablicy: " << tablica[i];
+			}			
+			else {
+				i--;
+			}
+		}
+		else {
+			cout << endl << "Debug isParzysta: " << isParzysta;
+			if (random % 2 == 0) {	
+				i--;
+				
+			}
+			else {
+				tablica[i] = random;
+				cout << endl << "Index: " << i << " w tablicy: "<<tablica[i];
+			}
+
+		}
+	} 
 }
