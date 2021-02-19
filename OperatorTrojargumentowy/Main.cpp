f96d73f3206c34b5df6f437811979639b0439c52
MultiApollo19 <mariusz.belwon@gmail.com>
2021-02-19 12:22:03 +0100

    Informatyczga

--- OperatorTrojargumentowy/Main.cpp
+++ OperatorTrojargumentowy/Main.cpp
@@ -13,5 +13,5 @@ int main() {
 
 	int najwieksza = a > b ? a : b;
 	cout << "Najwieksza liczba jest: " << najwieksza <<endl;
-
+	return 0;
 }
--- tablice/tablice.cpp
+++ tablice/tablice.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <cstdlib> 
 #include <time.h>
 
 using namespace std;
 int main() {
+	srand(time(NULL));
 	/*int tablica[10];
 	for (int i = 0; i < size(tablica); i++)
 	{
@@ -71,4 +72,53 @@ int main() {
 
 		}
 	} */
+
+	int tablica[10];
+	
+
+	for (int i = 0; i < size(tablica); i++)
+	{
+		int random = (rand() % 100) + 1;
+		cout << "Debug: "<< random << endl;
+		
+		if (random % 2==0) {
+			i--;
+		}
+		else {
+			tablica[i] = random;
+			cout << "Wartosc w tablicy: " << tablica[i] << endl;
+		}
+	}
+	return 0;
+	/* //Werjsa Pana Pryka
+	int t[10];
+	void wyswietl() { for (int i = 0; i < 10; i++) cout << t[i] << " "; cout << endl; }
+
+	//GENERUJEMY PARZYSTE Z PRZEDZIALU 1-100
+	srand(time(NULL));
+	//---------------------------wersja 1-------------------------
+	for (int i = 0; i < 10; i++) {
+		t[i] = rand() % 100 + 1;
+		if (t[i] % 2)i--;
+	}​​ wyswietl();
+	//---------------------------wersja 2-------------------------
+	for (int i = 0; i < 10; i++) do { t[i] = rand() % 100 + 1; } while (t[i] % 2); wyswietl();
+	//---------------------------wersja 3-------------------------
+	for (int i = 0; i < 10; i++) {
+		t[i] = rand() % 100 + 1;
+		(t[i] % 2) ? t[i]++ : t[i];
+	}​​ wyswietl();
+	//---------------------------wersja 4-------------------------
+	for (int i = 0; i < 10; i++) { t[i] = ((rand() % 99 + 2) / 2) * 2; }​​ wyswietl();
+	//---------------------------wersja 5-------------------------
+	for (int i = 0; i < 10; i++) { t[i] = 2 * (rand() % 50 + 1); }​​ wyswietl();
+	//---------------------------wersja 6-------------------------
+	for (int i = 0; i < 10; i++) { t[i] = rand() % 100 + 1; t[i] += t[i] % 2; }​​ wyswietl();
+
+
+
+
+
+	return 0;*/
+
 }
