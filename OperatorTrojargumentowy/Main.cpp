5f1e168f042218c8d8e47c70c0ad4e679a6bc8cb
MultiApollo19 <mariusz.belwon@gmail.com>
2021-02-19 10:17:05 +0100

    HEHE

---
+++ OperatorTrojargumentowy/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstdlib> 
+#include <time.h>
+
+using namespace std;
+int main() {
+	int a, b;
+	cout << "Podaj a: " << endl;
+	cin >> a;
+	cout << "Podaj b: " << endl;
+	cin >> b;
+
+	int najwieksza = a > b ? a : b;
+	cout << "Najwieksza liczba jest: " << najwieksza <<endl;
+
+}
--- tablice/tablice.cpp
+++ tablice/tablice.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <string>
 #include <cstdlib> 
+#include <time.h>
 
 using namespace std;
 int main() {
 	/*int tablica[10];
 	for (int i = 0; i < size(tablica); i++)
 	{
-		int random = rand();
-		cout << "Debug: " << random << endl;
-		if (random % 2 == 0) {
-			tablica[i] = 7;
+		srand(time(NULL));
+		int t[10];
+		for (int i = 0; i < 10; i += 2) {
+
+
+
+			t[i] = rand();
+			t[i + 1] = rand();
+
+
+
+
+			cout << t[i] << " - " << t[i + 1] << " - ";
+
+			if (t[i] % 2 == 0)t[i] = 7;
+			t[i + 1] = 1;
 		}
-		else {
-			tablica[i] = 1;
-		}
-		cout << tablica[i] << endl;
 	}*/
 
-
+	/*
 	int tablica[10];
 	bool isParzysta = false;
 	string tempInput;
@@ -61,5 +70,5 @@ int main() {
 			}
 
 		}
-	} 
+	} */
 }
