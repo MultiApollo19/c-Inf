4f1a371c0523767c5258c190b9086655c6030b9d
MultiApollo19 <mariusz.belwon@gmail.com>
2021-05-18 10:58:56 +0200

    Nowa rekurencyja
    ŁEŁO

--- Rekurencyja/Rekurencyja.cpp
+++ Rekurencyja/Rekurencyja.cpp
@@ -11,8 +11,31 @@ int silnia(int n) { //To jest ten tego no....yyyy...rekurencyjna funkcyja
 	if (n < 2) return 1;
 	return n * silnia(n - 1);
 }
+int ciag(int n){
+    switch(n){
+        case 1:
+            return 3;
+            break;
+        case 2:
+            return 5;
+            break;
+        default:
+            return ciag(n-1)*2+3;
+            break;
+    }
+
+}
 int main() {
-	//Tutaj s¹ pierdó³ki do tej pêtelki co tam bêdzie ni¿ej
+	    //Mamy dany ciÄ…g An okreslony rekurencyjnie a1=3;a2=5 an=a-n*2+3 napisz program sprawdzajacy ktory element ciagu jako 1 osiagnie wartosc wieksza od 100
+   	 int i =1;
+   	 while(ciag(i) <=1000){
+            cout << ciag(i)<<endl;
+            i++;
+    }
+	    cout << "Ciag wynosi powyzej 1000 przy n: " << i<< endl;
+
+	/*
+	//Tutaj sÂ¹ pierdÃ³Â³ki do tej pÃªtelki co tam bÃªdzie niÂ¿ej
 	int n; // <- TO jest ten.... nooo.... yyyyy.... kurde.... podstawa silni
 	int silniak = 1 ;// <- TO jest wynik...chyba?
 	cout << "Zapodaj N: ";
@@ -23,6 +46,6 @@ int main() {
 	}
 	cout << n << "! = " << silniak << endl;
 	cout << "Tera bd wyswietlac ta rekonkurencje" << endl;
-	cout << silnia(10) << endl; // TO CHIBA POLICZY TO TO, ale nie wiem
+	cout << silnia(10) << endl; // TO CHIBA POLICZY TO TO, ale nie wiem*/
 	
 }
