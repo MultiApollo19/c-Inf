c0e21acd6f431da0cd2563fab0c69aa3ac879326
MultiApollo19 <mariusz.belwon@gmail.com>
2021-09-21 12:11:18 +0200

    NWW & NWD
    Rozszerzony NWD, to brzmi jak dobry plan.

--- algorytmyooaaaoo/euklidesor.cpp
+++ algorytmyooaaaoo/euklidesor.cpp
@@ -2,13 +2,7 @@
 
 using namespace std;
 
-int main()
-{
-    //TO JE eNWuDe
-    int a, b;
-    cin >> a;
-    cin >> b;
-
+int nwd(int a, int b){
     while (a != b) {
         if (a > b) {
             a = a - b;
@@ -17,6 +11,29 @@ int main()
             b = b - a;
         }
     }
-    cout << "NWD: " << a;
+    return a;
+}
+
+int nwde(int a, int b){
+    int tmp;
+    while (b != 0) {tmp=a;a=b;b=tmp%b;    }
+    return a;
+}
+
+int main()
+{
+    
+    int a,b;
+    cin >> a;
+    cin >> b;
+
+    //TO JE eNWuWu
+    int nww;
+    nww = (a*b) / nwde(a,b);
+
+    cout << "NWD basic: " << nwd(a,b) <<endl;
+    cout << "NWD evolved: " << nwde(a,b)<<endl;
+    cout << "NWW: " << nww;
+
     return 0;
 }
