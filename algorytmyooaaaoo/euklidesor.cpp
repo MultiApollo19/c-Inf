cdb120ff339b76071087a0dd0db6d9709de6c63f
MultiApollo19 <mariusz.belwon@gmail.com>
2021-09-21 13:14:52 +0200

    Sitko :)

--- algorytmyooaaaoo/euklidesor.cpp
+++ algorytmyooaaaoo/euklidesor.cpp
@@ -22,8 +22,8 @@ int nwde(int a, int b){
 
 int main()
 {
-    
-    int a,b;
+    //Tutaj jest NWD i NWW wystarczy te blokowe wywalić i bd gitara
+    /*int a,b;
     cin >> a;
     cin >> b;
 
@@ -34,6 +34,28 @@ int main()
     cout << "NWD basic: " << nwd(a,b) <<endl;
     cout << "NWD evolved: " << nwde(a,b)<<endl;
     cout << "NWW: " << nww;
+    */
+    
+    //Tutaj jest takie sitko fajne, takie nie za dlugnie nie za krótkie, ale działające i elastyczne!
+    int n=0;
+    cout << "N: ";cin >> n;
 
+    bool liczby[n];
+    for (int i=0;i<=n;i++){
+        liczby[i] = true;
+    }
+    for (int i=2;i*i<=n; i++){
+        if(liczby[i]){
+            for(int j = i*2;j<=n;j=j+2){
+                liczby[j] = false;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++){
+        if(liczby[i]){
+            cout << i<< " ";
+        }
+    }
+    
     return 0;
 }
